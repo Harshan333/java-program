b1291e8f18df15e82218ee93083b0e67e1ab5eb4
Harshan <hgowdakh33@gmail.com>
2023-07-13 14:37:59 +0530

    check

---
+++ girias.java
@@ -0,0 +1,12 @@
+class Girias {
+
+public static void main (String arg[]){
+System.out.println("main started");
+String electronicdevices[] = {"refrigetator", "washng machine","relevision","led","grinder","ac","tronox","microwave"};
+//foreach
+for(String electronicdevice : electronicdevices){
+	System.out.println(electronicdevice);
+}
+System.out.println("main ended");
+}
+}
