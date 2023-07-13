a5db154d0c5e71fe4add34f67d99bef43a6f8fc6
Harshan <hgowdakh33@gmail.com>
2023-07-13 14:33:39 +0530

    check

---
+++ alchol.java
@@ -0,0 +1,12 @@
+class alchol{
+
+public static void main (String arg[]){
+System.out.println("mainstarteed");
+String brandnames[] = {"kingfisher","gyg","ygy"};
+
+for(String brandname: brandnames ){
+System.out.println(brandname);
+}
+System.out.println("main ended");
+}
+}
